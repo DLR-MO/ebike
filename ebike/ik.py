efda7ff09c1b67fcf6a6e9f17fb4c65e1df204bf
Timon Engelke <timon.engelke@dlr.de>
2024-08-19 11:17:21 +0200

    Add BioIKEmptyCostFnGoal2

--- ebike/ik.py
+++ ebike/ik.py
@@ -226,6 +226,7 @@ class BioIK(AbstractIK):
         reach_ros.set_parameter("reach_ros.use_line_goal", False)
         reach_ros.set_parameter("reach_ros.use_line_alignment", False)
         reach_ros.set_parameter("reach_ros.empty_cost_fn", False)
+        reach_ros.set_parameter("reach_ros.empty_cost_fn2", False)
         reach_ros.set_parameter("reach_ros.scan_goal", False)
         reach_ros.set_parameter("reach_ros.scan_with_offset", False)
         reach_ros.set_parameter("reach_ros.scan_swamp", False)
@@ -240,6 +241,14 @@ class BioIKEmptyCostFn(BioIK):
     def set_config(self, planning_group):
         super().set_config(planning_group)
         reach_ros.set_parameter("reach_ros.empty_cost_fn", True)
+
+
+class BioIKEmptyCostFn2(BioIK):
+    name = "BioIK (Empty CostFn2)"
+
+    def set_config(self, planning_group):
+        super().set_config(planning_group)
+        reach_ros.set_parameter("reach_ros.empty_cost_fn2", True)
 
 
 class BioIKRCM(BioIK):
